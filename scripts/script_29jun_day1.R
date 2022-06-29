52bba3fbe13977ec3de5d838f030f5a466dc8034
Mark Andrews <mjandrews.org@gmail.com>
2022-06-29 13:30:45 +0100

    Update script

--- scripts/script_29jun_day1.R
+++ scripts/script_29jun_day1.R
@@ -1,3 +1,19 @@
 library(mlr)
 library(tidyverse)
-      
+
+# Read in data ------------------------------------------------------------
+
+affairs_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/imdl03/main/data/affairs.csv")
+
+training_df <- affairs_df %>% 
+  # create binary target variable
+  mutate(affair = if_else(affairs > 0, 'yes', 'no')) %>%
+  # remove the `affairs` variable
+  select(-affairs) %>% 
+  # convert character vectors to factors
+  mutate(across(where(is.character), as.factor)) %>%
+  # relevel the target factor
+  mutate(affair = factor(affair, levels = c('yes', 'no'))) %>% 
+  # convert to base R data frame
+  as.data.frame()
+  
