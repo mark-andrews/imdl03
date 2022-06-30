7fcea0d1e6d9ee4bff479a3adb4fae95327e8ab7
Mark Andrews <mjandrews.org@gmail.com>
2022-06-30 19:39:23 +0100

    Update script

--- scripts/script_30jun_day2.R
+++ scripts/script_30jun_day2.R
@@ -232,7 +232,9 @@ convnet %>%
                 kernel_size = c(3, 3),
                 activation = 'relu') %>% 
   layer_max_pooling_2d(pool_size = c(2,2)) %>%
-  
+  layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = 'relu') %>%
+  layer_flatten() %>% 
+  layer_dense(units = 10, activation = 'softmax')
   
   
   
