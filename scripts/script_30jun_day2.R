85c895cba4ba88d7456439954908e229c6b51dbe
Mark Andrews <mjandrews.org@gmail.com>
2022-06-30 13:36:20 +0100

    Update script

--- scripts/script_30jun_day2.R
+++ scripts/script_30jun_day2.R
@@ -17,3 +17,40 @@ dtree_trained <- train(dtree, ZooTask)
 library(rpart.plot)
 dtree_M <- getLearnerModel(dtree_trained)
 rpart.plot(dtree_M)
+
+# evaluate performance
+p3 <- predict(dtree_trained, newdata = Zoo)
+calculateConfusionMatrix(p3)
+performance(p3, measures = list(acc, mmce))
+
+# Cross validation
+kfold_cv <- makeResampleDesc(method = 'RepCV', folds = 10, reps = 10)
+dtree_cv <- resample(dtree,
+                     task = ZooTask,
+                     resampling = kfold_cv,
+                     measures = list(acc, mmce))
+dtree_cv$aggr
+
+
+# Optimize the hyper-params -----------------------------------------------
+
+getParamSet(dtree)
+
+dtree_param_space <- makeParamSet(
+  makeIntegerParam('minsplit', lower = 3, upper = 25),
+  makeIntegerParam('minbucket', lower = 3, upper = 25),
+  makeNumericParam('cp', lower = 0.001, upper = 0.5),
+  makeIntegerParam('maxdepth', lower = 3, upper = 20)
+)
+
+rand_search <- makeTuneControlRandom(maxit = 100)
+
+kfold_cv <- makeResampleDesc(method = 'RepCV',
+                             folds = 10,
+                             reps = 10)
+
+dtree_tune <- tuneParams(dtree,
+                         task = ZooTask,
+                         resampling = kfold_cv,
+                         par.set = dtree_param_space,
+                         control = rand_search)
